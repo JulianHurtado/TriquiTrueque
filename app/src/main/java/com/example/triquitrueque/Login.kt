8aaa937f2ef3c0214c9ef79d4fff358fcd6422a1
jhurtado <hurtado0415@gmail.com>
2019-09-16 17:24:05 -0500

    DataBase with Realtime

--- app/src/main/java/com/example/triquitrueque/Login.kt
+++ app/src/main/java/com/example/triquitrueque/Login.kt
@@ -2,11 +2,25 @@ package com.example.triquitrueque
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 class Login : AppCompatActivity() {
 
+    private val mAuth: FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        //mAuth = FirebaseAuth.getInstance();
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
     }
 }
